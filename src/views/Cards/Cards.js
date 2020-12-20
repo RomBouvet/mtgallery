baae63ea67dc89e534390a458ca9f85f853b9e45
Romain B <bouvet.romain18@gmail.com>
2020-12-20 15:48:16 +0100

    Dynamic cards

--- src/views/Cards/Cards.js
+++ src/views/Cards/Cards.js
@@ -1,22 +1,63 @@
-import CardTile from '../../components/CardTile/CardTile'
-import './Cards.css'
+import { Component } from 'react';
+import CardTile from '../../components/CardTile/CardTile';
+import './Cards.css';
 
-export default function Cards() {
-  return (
-    <div class="w-full grid grid-cols-auto-fill-200 gap-4 pr-4 pl-4">
-      <CardTile id="433289"/>
-      <CardTile id="433290"/>
-      <CardTile id="433291"/>
-      <CardTile id="433292"/>
-      <CardTile id="433293"/>
-      <CardTile id="433294"/>
-      <CardTile id="433295"/>
-      <CardTile id="433296"/>
-      <CardTile id="433297"/>
-      <CardTile id="433298"/>
-      <CardTile id="433299"/>
-      <CardTile id="433300"/>
-      <CardTile id="433301"/>
-    </div>
-  );
+export default class Cards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      cards: []
+    };
+  }
+
+  componentDidMount() {
+    console.log("FETCHING")
+    fetch("http://localhost:8080/cards/", {
+      // mode: 'no-cors',
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      }
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log("I AM HERE")
+          this.setState({
+            isLoaded: true,
+            cards: result
+          });
+        },
+        // Remarque : il est important de traiter les erreurs ici
+        // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+        // des exceptions provenant de réels bugs du composant.
+        (error) => {
+          console.log(error)
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, cards } = this.state;
+    if (error) {
+      console.log(error);
+      return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Chargement…</div>;
+    } else {
+      return (
+        <div className="w-full grid grid-cols-auto-fill-200 gap-4 pr-4 pl-4">
+          {cards.map(card => (
+            <CardTile id={card.id} />
+          ))}
+        </div>
+      );
+    }
+  }
 }
